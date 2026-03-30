0e3c45396d4e19c23072c1ea810f6d841177d395
SiddharthaMukherji <siddhartha.mukherji55@gmail.com>
2026-03-30 10:52:19 +0000

    w

--- email-reminder/config.py
+++ email-reminder/config.py
@@ -2,9 +2,9 @@ SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
 
 SENDER_EMAIL = "siddhartha.mukherji55@gmail.com"
-SENDER_PASSWORD = "bvlwtltkguenzfuw"  # Use app password, not real password
+SENDER_PASSWORD = "bgkagxbpyxvgknnv"  # Use app password, not real password
 
 RECIPIENT_EMAIL = "grievanceredressal@nivabupa.com","gro@nivabupa.com"
 
-MAX_REMINDERS = 20  # n value
+MAX_REMINDERS = 200  # n value
 COUNTER_FILE = "counter.txt"
